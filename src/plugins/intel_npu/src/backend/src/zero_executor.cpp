be34b701fc583fec2af4a5ee9d91a14c67730c7b
Liu, Dan1 <dan1.liu@intel.com>
2024-11-05 09:34:10 +0800

    fix build
    Signed-off-by: Liu, Dan1 <dan1.liu@intel.com>

--- src/plugins/intel_npu/src/backend/src/zero_executor.cpp
+++ src/plugins/intel_npu/src/backend/src/zero_executor.cpp
@@ -14,10 +14,10 @@
 #include "intel_npu/common/itt.hpp"
 #include "intel_npu/config/common.hpp"
 #include "intel_npu/prefix.hpp"
+#include "intel_npu/utils/zero/zero_utils.hpp"
 #include "openvino/runtime/properties.hpp"
 #include "ze_command_queue_npu_ext.h"
 #include "zero_device.hpp"
-#include "zero_utils.hpp"
 
 using namespace intel_npu;
 
