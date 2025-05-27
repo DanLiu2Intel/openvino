1f0a5e648c59388fe7610ebef7016610a99def7c
Xin Wang <xin1.wang@intel.com>
2025-05-27 15:49:44 +0800

    Use driver ext to parse metadata
    Signed-off-by: Xin Wang <xin1.wang@intel.com>

--- src/plugins/intel_npu/src/compiler_adapter/src/plugin_compiler_adapter.cpp
+++ src/plugins/intel_npu/src/compiler_adapter/src/plugin_compiler_adapter.cpp
@@ -102,16 +102,26 @@ std::shared_ptr<IGraph> PluginCompilerAdapter::compile(const std::shared_ptr<con
     ov::Tensor tensor = make_tensor_from_vector(networkDesc.compiledNetwork);
     GraphDescriptor graphDesc;
 
+    NetworkMetadata networkMeta;
     if (_zeGraphExt) {
         // Depending on the config, we may get an error when trying to get the graph handle from the compiled
         // network
         try {
+<<<<<<< HEAD
             graphDesc = _zeGraphExt->getGraphDescriptor(tensor.data(), tensor.get_byte_size());
+=======
+            graphHandle =
+                _zeGraphExt->getGraphHandle(*reinterpret_cast<const uint8_t*>(tensor.data()), tensor.get_byte_size());
+
+            networkMeta = _zeGraphExt->getNetworkMeta(graphHandle);
+            networkMeta.name = model->get_friendly_name();
+>>>>>>> 9a58e41803 (Use driver ext to parse metadata)
         } catch (...) {
             _logger.info("Failed to obtain the level zero graph handle. Inference requests for this model are not "
                          "allowed. Only exports are available");
         }
     }
+<<<<<<< HEAD
 
     return std::make_shared<Graph>(
         _zeGraphExt,
@@ -122,6 +132,25 @@ std::shared_ptr<IGraph> PluginCompilerAdapter::compile(const std::shared_ptr<con
         config,
         /* persistentBlob = */ true,  // exporting the blob shall be available in such a scenario
         _compiler);
+=======
+    return std::make_shared<Graph>(_zeGraphExt,
+                                   _zeroInitStruct,
+                                   graphHandle,
+                                   std::move(networkMeta),
+                                   std::move(tensor),
+                                   /* blobAllocatedByPlugin = */ false,
+                                   config,
+                                   _compiler);
+
+    // return std::make_shared<Graph>(_zeGraphExt,
+    //                                _zeroInitStruct,
+    //                                graphHandle,
+    //                                std::move(networkDesc.metadata),
+    //                                std::move(tensor),
+    //                                /* blobAllocatedByPlugin = */ false,
+    //                                config,
+    //                                _compiler);
+>>>>>>> 9a58e41803 (Use driver ext to parse metadata)
 }
 
 std::shared_ptr<IGraph> PluginCompilerAdapter::compileWS(const std::shared_ptr<ov::Model>& model,
@@ -267,6 +296,7 @@ std::shared_ptr<IGraph> PluginCompilerAdapter::parse(
 
     GraphDescriptor mainGraphDesc;
 
+    NetworkMetadata networkMeta;
     if (_zeGraphExt) {
         mainGraphDesc = _zeGraphExt->getGraphDescriptor(mainBlob.data(), mainBlob.get_byte_size());
     }
