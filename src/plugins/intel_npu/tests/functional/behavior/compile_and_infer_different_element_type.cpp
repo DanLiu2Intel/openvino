162b83ee4c0336a5d82dfb18d0024a2f65f90807
Liu, Dan1 <dan1.liu@intel.com>
2025-06-30 18:34:59 +0800

    fix

--- src/plugins/intel_npu/tests/functional/behavior/compile_and_infer_different_element_type.cpp
+++ src/plugins/intel_npu/tests/functional/behavior/compile_and_infer_different_element_type.cpp
@@ -12,45 +12,45 @@
 /**
  * Reads configuration environment variables
  */
-class VpuTestEnvConfig {
-public:
-    std::string IE_NPU_TESTS_DEVICE_NAME;
-    std::string IE_NPU_TESTS_DUMP_PATH;
-    std::string IE_NPU_TESTS_LOG_LEVEL;
-    std::string IE_NPU_TESTS_PLATFORM;
-    std::string OV_NPU_TESTS_SKIP_CONFIG_FILE = "npu_skip_func_tests.xml";
+// class VpuTestEnvConfig {
+// public:
+//     std::string IE_NPU_TESTS_DEVICE_NAME;
+//     std::string IE_NPU_TESTS_DUMP_PATH;
+//     std::string IE_NPU_TESTS_LOG_LEVEL;
+//     std::string IE_NPU_TESTS_PLATFORM;
+//     std::string OV_NPU_TESTS_SKIP_CONFIG_FILE = "npu_skip_func_tests.xml";
 
-    bool IE_NPU_TESTS_RUN_COMPILER = true;
-    bool IE_NPU_TESTS_RUN_EXPORT = false;
-    bool IE_NPU_TESTS_RUN_IMPORT = false;
-    bool IE_NPU_TESTS_RUN_INFER = true;
-    bool IE_NPU_TESTS_EXPORT_INPUT = false;
-    bool IE_NPU_TESTS_EXPORT_OUTPUT = false;
-    bool IE_NPU_TESTS_EXPORT_REF = false;
-    bool IE_NPU_TESTS_IMPORT_INPUT = false;
-    bool IE_NPU_TESTS_IMPORT_REF = false;
-    bool IE_NPU_SINGLE_CLUSTER_MODE = false;
+//     bool IE_NPU_TESTS_RUN_COMPILER = true;
+//     bool IE_NPU_TESTS_RUN_EXPORT = false;
+//     bool IE_NPU_TESTS_RUN_IMPORT = false;
+//     bool IE_NPU_TESTS_RUN_INFER = true;
+//     bool IE_NPU_TESTS_EXPORT_INPUT = false;
+//     bool IE_NPU_TESTS_EXPORT_OUTPUT = false;
+//     bool IE_NPU_TESTS_EXPORT_REF = false;
+//     bool IE_NPU_TESTS_IMPORT_INPUT = false;
+//     bool IE_NPU_TESTS_IMPORT_REF = false;
+//     bool IE_NPU_SINGLE_CLUSTER_MODE = false;
 
-    bool IE_NPU_TESTS_RAW_EXPORT = false;
-    bool IE_NPU_TESTS_LONG_FILE_NAME = false;
+//     bool IE_NPU_TESTS_RAW_EXPORT = false;
+//     bool IE_NPU_TESTS_LONG_FILE_NAME = false;
 
-public:
-    static const VpuTestEnvConfig& getInstance();
+// public:
+//     static const VpuTestEnvConfig& getInstance();
 
-private:
-    explicit VpuTestEnvConfig();
-};
+// private:
+//     explicit VpuTestEnvConfig();
+// };
 
-std::string getTestsPlatformFromEnvironmentOr(const std::string& instead) {
-    return (!VpuTestEnvConfig::getInstance().IE_NPU_TESTS_PLATFORM.empty())
-                   ? VpuTestEnvConfig::getInstance().IE_NPU_TESTS_PLATFORM
-                   : instead;
-}
+// std::string getTestsPlatformFromEnvironmentOr(const std::string& instead) {
+//     return (!VpuTestEnvConfig::getInstance().IE_NPU_TESTS_PLATFORM.empty())
+//                    ? VpuTestEnvConfig::getInstance().IE_NPU_TESTS_PLATFORM
+//                    : instead;
+// }
 
-std::string getTestsPlatformCompilerInPlugin() {
-    return getTestsPlatformFromEnvironmentOr(
-            getTestsDeviceNameFromEnvironmentOr(std::string(ov::intel_npu::Platform::AUTO_DETECT)));
-}
+// std::string getTestsPlatformCompilerInPlugin() {
+//     return getTestsPlatformFromEnvironmentOr(
+//             getTestsDeviceNameFromEnvironmentOr(std::string(ov::intel_npu::Platform::AUTO_DETECT)));
+// }
 
 // auto configs = []() {
 //     return std::vector<ov::AnyMap>{{{ov::intel_npu::compiler_type(ov::intel_npu::CompilerType::MLIR),
@@ -90,7 +90,7 @@ auto configs2 = []() {
 };
 
 INSTANTIATE_TEST_SUITE_P(
-        smoke_BehaviorTests, NPUInferRequestElementTypeTests,
+        smoke_BehaviorTests_3720, NPUInferRequestElementTypeTests,
         ::testing::Combine(::testing::Values(getFunction()),
                            ::testing::Values(std::vector<std::pair<std::vector<size_t>, std::vector<size_t>>>{
                                    {{1, 1, 128}, {1, 1, 128}}, {{128}, {128}}}),
@@ -98,7 +98,7 @@ INSTANTIATE_TEST_SUITE_P(
         ov::test::utils::appendPlatformTypeTestName<OVInferRequestDynamicTests>);
 
 INSTANTIATE_TEST_SUITE_P(
-        smoke_BehaviorTests, NPUInferRequestElementTypeTests,
+        smoke_BehaviorTests_4000, NPUInferRequestElementTypeTests,
         ::testing::Combine(::testing::Values(getFunction()),
                            ::testing::Values(std::vector<std::pair<std::vector<size_t>, std::vector<size_t>>>{
                                    {{1, 1, 128}, {1, 1, 128}}, {{128}, {128}}}),
