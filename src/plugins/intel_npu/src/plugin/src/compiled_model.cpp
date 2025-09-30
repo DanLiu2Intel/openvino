888c82f5f803aa8e587ca0df8f32092d25c0edcb
Liu, Dan1 <dan1.liu@intel.com>
2025-09-30 15:00:08 +0800

    add import compare

--- src/plugins/intel_npu/src/plugin/src/compiled_model.cpp
+++ src/plugins/intel_npu/src/plugin/src/compiled_model.cpp
@@ -13,13 +13,13 @@
 #include "intel_npu/config/config.hpp"
 #include "intel_npu/config/options.hpp"
 #include "metadata.hpp"
+#include "openvino/op/constant.hpp"
+#include "openvino/op/parameter.hpp"
+#include "openvino/op/result.hpp"
 #include "openvino/pass/constant_folding.hpp"
 #include "openvino/runtime/properties.hpp"
 #include "openvino/runtime/system_conf.hpp"
 #include "openvino/runtime/threading/executor_manager.hpp"
-#include "openvino/op/constant.hpp"
-#include "openvino/op/parameter.hpp"
-#include "openvino/op/result.hpp"
 
 #if defined(__linux__)
 #    include "transformations/utils/utils.hpp"
@@ -123,6 +123,74 @@ std::string print_all_info(const std::shared_ptr<ov::Model>& model) {
     return ss.str();
 }
 
+std::string print_parameters2(const std::shared_ptr<ov::Model>& model) {
+    std::ostringstream ss;
+    ss << "=== Model Parameters (Inputs) ===" << std::endl;
+    const auto& parameters = model->get_parameters();
+    ss << "Total Parameters: " << parameters.size() << std::endl;
+
+    for (size_t i = 0; i < parameters.size(); ++i) {
+        const auto& param = parameters[i];
+        // ss << "  [" << i << "] " << param->get_friendly_name() << "/(get_name is " << param->get_name()
+        //    << ") : "
+        ss << param->get_element_type() << " " << param->get_partial_shape() << std::endl;
+
+        // Print additional parameter info
+        ss << "      Type: " << param->get_type_name() << std::endl;
+        if (param->get_output_size() > 0) {
+            ss << "      Output tensor names (may contains multi names): ";
+            for (size_t j = 0; j < param->get_output_size(); ++j) {
+                auto names = param->get_output_tensor(j).get_names();
+                for (const auto& name : names) {
+                    ss << name << " ";
+                }
+            }
+            ss << std::endl;
+        }
+    }
+    ss << std::endl;
+    return ss.str();
+}
+
+std::string print_results2(const std::shared_ptr<ov::Model>& model) {
+    std::ostringstream ss;
+    ss << "=== Model Results (Outputs) ===" << std::endl;
+    const auto& results = model->get_results();
+    ss << "Total Results: " << results.size() << std::endl;
+
+    for (size_t i = 0; i < results.size(); ++i) {
+        const auto& result = results[i];
+        ss << "  [" << i << "] "
+           << result->get_friendly_name()
+              // << "/(get_name is " << result->get_name()
+              // << ") : " << std::endl;
+           << "      Type: " << result->get_type_name() << std::endl;
+
+        if (result->get_input_size() > 0) {
+            const auto& input = result->get_input_source_output(0);
+            ss << "      Element Type: " << input.get_element_type() << std::endl;
+            ss << "      Shape: " << input.get_partial_shape() << std::endl;
+
+            auto names = result->get_output_tensor(0).get_names();
+            if (!names.empty()) {
+                ss << "      Tensor names (may contains multi names):: ";
+                for (const auto& name : names) {
+                    ss << name << " ";
+                }
+                ss << std::endl;
+            }
+        }
+#if defined(__linux__)
+        /// new add test line
+        ss << "     Output tensor name (ov::op::util::get_ie_output_name(result->input_value(0))): "
+           << ov::op::util::get_ie_output_name(result->input_value(0)) << std::endl;
+#endif
+    }
+    ss << std::endl;
+
+    return ss.str();
+}
+
 std::string compare_info(const std::shared_ptr<ov::Model>& model) {
     if (!model) {
         std::cout << "Model is null!" << std::endl;
@@ -133,8 +201,8 @@ std::string compare_info(const std::shared_ptr<ov::Model>& model) {
     ss << "Graph Size: " << model->get_graph_size() << " bytes" << std::endl;
     ss << "Is Dynamic: " << (model->is_dynamic() ? "Yes" : "No") << std::endl;
     ss << std::endl;
-    ss << print_parameters(model);
-    ss << print_results(model);
+    ss << print_parameters2(model);
+    ss << print_results2(model);
     return ss.str();
 }
 
@@ -290,12 +358,12 @@ std::shared_ptr<const ov::Model> CompiledModel::get_runtime_model() const {
 
     auto modelUseMetadata = std::make_shared<ov::Model>(resultMetatdasMetatda, parameterMetatdasMetatda);
     try {
+        std::cout << "------------modelNoUseMetadata----------------" << std::endl;
+        std::cout << print_all_info(modelNoUseMetadata) << std::endl;
+        std::cout << "-----------modelNoUseMetadata VS modelUseMetadata-----------------" << std::endl;
+        std::cout << print_all_info(modelUseMetadata) << std::endl;
+        std::cout << "------------modelUseMetadata----------------" << std::endl;
         if (compare_info(modelNoUseMetadata) != compare_info(modelUseMetadata)) {
-            std::cout << "------------modelNoUseMetadata----------------" << std::endl;
-            std::cout << print_all_info(modelNoUseMetadata) << std::endl;
-            std::cout << "-----------modelNoUseMetadata VS modelUseMetadata-----------------" << std::endl;
-            std::cout << print_all_info(modelUseMetadata) << std::endl;
-            std::cout << "------------modelUseMetadata----------------" << std::endl;
             throw NotEqualException(
                 "Runtime model using metadata and not using's result are NOT EQUAL in get_runtime_model()");
         } else {
