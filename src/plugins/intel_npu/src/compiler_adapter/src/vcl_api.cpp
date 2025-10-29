c7181f3798373b29240ab3ec964bccb12c56e1e3
Liu, Dan1 <dan1.liu@intel.com>
2025-10-30 01:44:31 +0800

    fix

--- src/plugins/intel_npu/src/compiler_adapter/src/vcl_api.cpp
+++ src/plugins/intel_npu/src/compiler_adapter/src/vcl_api.cpp
@@ -199,7 +199,7 @@ void VCLCompilerImpl::updateVCLCompilerCreate(const std::string platform) {
     std::cout << "  ====> updateVCLCompilerCreate called, platform: " << platform << std::endl;
     std::map<std::string, vcl_device_desc_t> device_configs = {
         {"NPU.4000",{sizeof(vcl_device_desc_t), 0x643E, 0, 5}}, //4000
-        {"NPU.3720",{sizeof(vcl_device_desc_t), 0x7D1D, 0 1}} // 3720, need to be updated tile
+        {"NPU.3720",{sizeof(vcl_device_desc_t), 0x7D1D, 0, 1}} // 3720, need to be updated tile
         //error: narrowing conversion of ‘-1’ from ‘int’ to ‘uint16_t’ {aka ‘short unsigned int’} [-Wnarrowing]
         //{{sizeof(vcl_device_desc_t), 0xAD1D, -1, 1}}  // 3720, need to be updated tile
     };
