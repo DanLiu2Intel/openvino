a77d964b2d1a545f9bebf9f7100416ea6b9b87ad
Liu, Dan1 <dan1.liu@intel.com>
2026-06-01 19:07:28 +0800

    test1

--- src/plugins/intel_npu/src/backend/src/zero_device.cpp
+++ src/plugins/intel_npu/src/backend/src/zero_device.cpp
@@ -4,7 +4,6 @@
 
 #include "zero_device.hpp"
 
-#include "intel_npu/common/idynamic_graph.hpp"
 #include "intel_npu/common/itt.hpp"
 #include "intel_npu/utils/zero/zero_api.hpp"
 #include "intel_npu/utils/zero/zero_utils.hpp"
