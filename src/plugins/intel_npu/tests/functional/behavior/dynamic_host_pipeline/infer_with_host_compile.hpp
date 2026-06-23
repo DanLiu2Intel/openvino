54b103bb0e2ee4fc4558817f88672fe0d3aaf777
Liu, Dan1 <dan1.liu@intel.com>
2026-06-23 15:35:34 +0800

    add test for layout and COMPILATION_MODE_PARAMS

--- src/plugins/intel_npu/tests/functional/behavior/dynamic_host_pipeline/infer_with_host_compile.hpp
+++ src/plugins/intel_npu/tests/functional/behavior/dynamic_host_pipeline/infer_with_host_compile.hpp
@@ -101,6 +101,7 @@ inline std::shared_ptr<ov::Model> createMultiplyModel() {
     // making input and output to be NHWC
     const char* env_p = std::getenv("MY_APP_MODE");
     if (env_p == nullptr) {
+        std::cout << "MY_APP_MODE is NOT set, Use layput NHWC" << std::endl;
         auto preProc = ov::preprocess::PrePostProcessor(model);
         preProc.input(0).tensor().set_layout("NHWC");
         preProc.input(0).model().set_layout("NCHW");
@@ -108,6 +109,8 @@ inline std::shared_ptr<ov::Model> createMultiplyModel() {
         preProc.output(0).model().set_layout("NCHW");
 
         model = preProc.build();
+    } else {
+        std::cout << "MY_APP_MODE is set, Use layput NCHW" << std::endl;
     }
 
     return model;
